5bff56faa14f46d2ad09b61c51b7dcbd394c2b13
Jaspher Jan Pasol <jpasol@sbitc.ph>
2018-10-30 11:18:05 +0800

    Named Controls

--- Budgeted Volume Creator/frmBudget.Designer.vb
+++ Budgeted Volume Creator/frmBudget.Designer.vb
@@ -22,32 +22,30 @@ Partial Class frmBudget
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.tabYear = New System.Windows.Forms.TabControl()
         Me.TabPage1 = New System.Windows.Forms.TabPage()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.dgvBudget = New System.Windows.Forms.DataGridView()
         Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.TabPage2 = New System.Windows.Forms.TabPage()
         Me.Button1 = New System.Windows.Forms.Button()
-        Me.TabControl1.SuspendLayout()
+        Me.tabYear.SuspendLayout()
         Me.TabPage1.SuspendLayout()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.dgvBudget, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'TabControl1
+        'tabYear
         '
-        Me.TabControl1.Controls.Add(Me.TabPage1)
-        Me.TabControl1.Controls.Add(Me.TabPage2)
-        Me.TabControl1.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!)
-        Me.TabControl1.Location = New System.Drawing.Point(12, 12)
-        Me.TabControl1.Name = "TabControl1"
-        Me.TabControl1.SelectedIndex = 0
-        Me.TabControl1.Size = New System.Drawing.Size(1088, 705)
-        Me.TabControl1.TabIndex = 0
+        Me.tabYear.Controls.Add(Me.TabPage1)
+        Me.tabYear.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!)
+        Me.tabYear.Location = New System.Drawing.Point(12, 12)
+        Me.tabYear.Name = "tabYear"
+        Me.tabYear.SelectedIndex = 0
+        Me.tabYear.Size = New System.Drawing.Size(1088, 705)
+        Me.tabYear.TabIndex = 0
         '
         'TabPage1
         '
-        Me.TabPage1.Controls.Add(Me.DataGridView1)
+        Me.TabPage1.Controls.Add(Me.dgvBudget)
         Me.TabPage1.Location = New System.Drawing.Point(4, 29)
         Me.TabPage1.Name = "TabPage1"
         Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
@@ -56,14 +54,14 @@ Partial Class frmBudget
         Me.TabPage1.Text = "TabPage1"
         Me.TabPage1.UseVisualStyleBackColor = True
         '
-        'DataGridView1
+        'dgvBudget
         '
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2})
-        Me.DataGridView1.Location = New System.Drawing.Point(6, 6)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(1068, 660)
-        Me.DataGridView1.TabIndex = 2
+        Me.dgvBudget.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvBudget.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2})
+        Me.dgvBudget.Location = New System.Drawing.Point(6, 6)
+        Me.dgvBudget.Name = "dgvBudget"
+        Me.dgvBudget.Size = New System.Drawing.Size(1068, 660)
+        Me.dgvBudget.TabIndex = 2
         '
         'Column1
         '
@@ -75,16 +73,6 @@ Partial Class frmBudget
         Me.Column2.HeaderText = "Budgeted Volume"
         Me.Column2.Name = "Column2"
         '
-        'TabPage2
-        '
-        Me.TabPage2.Location = New System.Drawing.Point(4, 29)
-        Me.TabPage2.Name = "TabPage2"
-        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage2.Size = New System.Drawing.Size(1080, 672)
-        Me.TabPage2.TabIndex = 1
-        Me.TabPage2.Text = "TabPage2"
-        Me.TabPage2.UseVisualStyleBackColor = True
-        '
         'Button1
         '
         Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!)
@@ -92,7 +80,7 @@ Partial Class frmBudget
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(177, 76)
         Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Save | Print"
+        Me.Button1.Text = "cmdSave"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'frmBudget
@@ -101,24 +89,23 @@ Partial Class frmBudget
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1350, 729)
         Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.TabControl1)
+        Me.Controls.Add(Me.tabYear)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "frmBudget"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
         Me.Text = "Form1"
         Me.TopMost = True
-        Me.TabControl1.ResumeLayout(False)
+        Me.tabYear.ResumeLayout(False)
         Me.TabPage1.ResumeLayout(False)
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dgvBudget, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
-    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents tabYear As TabControl
     Friend WithEvents TabPage1 As TabPage
-    Friend WithEvents TabPage2 As TabPage
     Friend WithEvents Button1 As Button
-    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents dgvBudget As DataGridView
     Friend WithEvents Column1 As DataGridViewTextBoxColumn
     Friend WithEvents Column2 As DataGridViewTextBoxColumn
 End Class
